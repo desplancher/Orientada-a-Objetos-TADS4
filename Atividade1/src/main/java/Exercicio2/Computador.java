ff7c06f0065d12bb7dfe8abf298b2747d9ca4845
Desplancher <joaodesplancher@hotmail.com>
2023-02-19 09:58:43 -0300

    Criado Classe Computador

---
+++ Atividade1/src/main/java/Exercicio2/Computador.java
@@ -0,0 +1,20 @@
+
+package Exercicio2;
+
+public class Computador {
+    String marca, cor, modelo;
+    int nSerie;
+    double preco;
+    
+    void imprimir(){
+        
+    }
+    
+    void calcularValor(){
+        
+    }
+    
+    void alterarValor(){
+        
+    }
+}
